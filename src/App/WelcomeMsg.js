5702d0dbe53374c660bd99b475924446eaf7cc99
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-10-06 20:43:32 -0700

    created appbar

--- src/App/WelcomeMsg.js
+++ src/App/WelcomeMsg.js
@@ -1,5 +1,5 @@
-import React from 'react';
+import React from "react";
 
 export default function Welcome() {
-    return <h1>Welcome to CryptoBoard</h1>
+  return <h1>Welcome to CryptoBoard</h1>;
 }
--- src/App/index.js
+++ src/App/index.js
@@ -1,18 +1,18 @@
-import React, {Component} from 'react';
-import './App.css';
-import Welcome from './WelcomeMsg';
-import styled, {css} from 'styled-components';
-import AppLayout from './AppLayout'
-
+import React, { Component } from "react";
+import "./App.css";
+import Welcome from "./WelcomeMsg";
+import styled, { css } from "styled-components";
+import AppLayout from "./AppLayout";
+import AppBar from "./AppBar";
 
 class App extends Component {
   render() {
     return (
       <AppLayout>
+        <AppBar />
         <Welcome />
-
       </AppLayout>
-    )
+    );
   }
 }
 
