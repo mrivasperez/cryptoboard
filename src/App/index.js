3ccccf161b9a616bed25506e40a1f184a8ced844
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-10-06 20:34:21 -0700

    imported font

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 import './App.css';
+import Welcome from './WelcomeMsg';
+import styled, {css} from 'styled-components';
+
+
 
 class App extends Component {
   render() {
     return (
       <div>
-      Hello World!
+        <Welcome />
+
       </div>
     )
   }
