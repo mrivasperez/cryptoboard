cf3f8c1d3bdf567a0e11ce52fb9576d1a4735c2e
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-10-17 15:16:19 -0700

    add page

---
+++ src/App/AppProvider.js
@@ -0,0 +1,59 @@
+//state manager
+import React from "react";
+const cc = require("cryptocompare");
+cc.setApiKey(
+  "a786a553e1ed1dd59b185ebf1c097a9b6ddae7da68de54f83a9b84ecaa741a35"
+);
+export const AppContext = React.createContext();
+
+export class AppProvider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: "dashboard",
+      ...this.savedSettings(),
+      setPage: this.setPage,
+      confirmFavorites: this.confirmFavorites,
+    };
+  }
+
+  componentDidMount = () => {
+    this.fetchCoins();
+  };
+
+  fetchCoins = async () => {
+    let coinList = (await cc.coinList()).Data;
+    console.log(coinList);
+  };
+
+  confirmFavorites = () => {
+    this.setState({
+      firstVisit: false,
+      page: "dashboard",
+    });
+    localStorage.setItem(
+      "cryptoDash",
+      JSON.stringify({
+        test: "hello",
+      })
+    );
+  };
+
+  savedSettings() {
+    let cryptoDashData = JSON.parse(localStorage.getItem("cryptoDash"));
+    if (!cryptoDashData) {
+      return { page: "settings", firstVisit: true };
+    }
+    return {};
+  }
+
+  setPage = page => this.setState({ page });
+
+  render() {
+    return (
+      <AppContext.Provider value={this.state}>
+        {this.props.children}
+      </AppContext.Provider>
+    );
+  }
+}
